0f015fbb9b8473342470915ccfe0db8d020b7051
Jeiji7 <egorpopelyuk43@gmail.com>
2023-11-26 23:39:56 +0300

    Регистрация доделана отмечаем всей маршруткой ура ура ура

--- GamerShopStore/Pages/Enter.xaml.cs
+++ GamerShopStore/Pages/Enter.xaml.cs
@@ -12,22 +12,43 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using GamerShopStore.BDSHKA;
+
 
 namespace GamerShopStore.Pages
 {
-    /// <summary>
-    /// Логика взаимодействия для Enter.xaml
-    /// </summary>
+   
     public partial class Enter : Page
     {
+        public static Employee empl;
+        public static List<Employee> employee { get; set; }
+
         public Enter()
         {
+           
             InitializeComponent();
         }
 
         private void Button_Click_Enter(object sender, RoutedEventArgs e)
         {
-            NavigationService.Navigate(new Supervisor());
+            string _login = LoginTB.Text.Trim();
+            string _password = PasswordPS.Password.Trim();
+            employee = new List<Employee>(App.BD.Employee.ToList());
+            Employee currectUser = employee.FirstOrDefault(x => x.Login == _login && x.Password == _password);
+            empl = currectUser;
+            if (currectUser != null)
+            {
+                MessageBox.Show($"Добро пожаловать {empl.Name} !!!");
+                if (empl.ID_post == 1)
+                    NavigationService.Navigate(new Supervisor());
+                else if (empl.ID_post == 2)
+                    NavigationService.Navigate(new Supervisor());
+                else if (empl.ID_post == 3)
+                    NavigationService.Navigate(new Supervisor());
+
+            }
+            else
+                MessageBox.Show("Неверный Логин или пароль!!!");
         }
     }
 }
